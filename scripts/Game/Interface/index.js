ef9d1e40187e76d534471c89c99ae9d23441862f
ThomasBackens <thomas.backens@yahoo.com>
2021-02-12 17:11:00 -0600

    added ui

--- scripts/Game/Interface/index.js
+++ scripts/Game/Interface/index.js
@@ -5,28 +5,33 @@ function Interface(config) {
     strokeWeight(3);
     stroke(0, 255, 255, 200);
     fill(0, 255, 255, 50);
-    rect(windowWidth / 2, windowHeight - 100, windowWidth / 1.5, 40, 10);
+    rect(windowWidth / 2, windowHeight - 100, windowWidth / 2.5, 40, 10);
 
     for (let i = 0; i < this.character.abilities.length; i++) {
       let abilityX =
         windowWidth / 2 -
-        windowWidth / 2 / 2 +
-        (windowWidth / 2 / (this.character.abilities.length - 1)) * i;
-      let abilityY = windowHeight - 160;
+        windowWidth / 2 / 3.5 +
+        (windowWidth / 3.5 / (this.character.abilities.length - 1)) * i;
+      let abilityY = windowHeight - 150;
 
       noStroke();
       textAlign(CENTER, CENTER);
       textSize(20);
       fill(0, 255, 255);
-      text(this.character.abilities[i].name, abilityX, abilityY - 110);
+      text(this.character.abilities[i].name, abilityX, abilityY - 100);
       strokeWeight(3);
       stroke(0, 255, 255, 200);
       fill(0, 255, 255, 50);
-      rect(abilityX, abilityY, 120, 120, 20);
-      rect(abilityX, abilityY - 75, 70, 30, 10, 10, 0, 0);
+      rect(abilityX, abilityY, 100, 100, 20);
+      rect(abilityX, abilityY - 65, 50, 30, 10, 10, 0, 0);
       noStroke();
       fill(0, 255, 255);
-      text(this.character.abilities[i].key, abilityX, abilityY - 75);
+      text(this.character.abilities[i].key, abilityX, abilityY - 65);
     }
+
+    noStroke();
+    fill(200, 100, 0);
+    rect(50, windowHeight - 50, 100, 100);
+    rect(windowWidth - 150, windowHeight - 150, 300, 300);
   };
 }
